1ded478a14de4daf087a9476ebd524c58becb382
alhmami-nabil <nabil.alhmami.solicode@gmail.com>
2021-06-04 11:46:21 +0100

    site complet

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1623403721;s:7:"authKey";s:64:"$lSi$lYu)Vr FM2MQ/U8)[pcfNOa<aB[7s,bl0N4uHrdv9hsk$2S?5aB5JM)T9_#";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3079;}
